351417239328777b8fb295635069f7e84539b76b
malaynayak <malay.kanta.nayak@gmail.com>
2022-06-19 00:44:48 +0530

    Fixed tests.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App
+          GH Workflow
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/gh workflow/i);
   expect(linkElement).toBeInTheDocument();
 });
