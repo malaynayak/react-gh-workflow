ed841683da221f015d79992333d4a3964e681c39
Malay Nayak <malay.kanta.nayak@gmail.com>
2022-08-13 17:00:37 +0530

    feat: jS Action (#15)
    feat: Added custom action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = [core.getInput("assignees")];
+
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees,
+    });
+    core.setOutput("issue", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+run();
